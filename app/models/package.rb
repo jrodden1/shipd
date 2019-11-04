530ff4edb40c9b3f53888cec5a66d1669918c6f7
Jeremiah Rodden <jrodden1@gmail.com>
2019-11-04 15:33:28 -0700

    Added Model Validations

--- app/models/package.rb
+++ app/models/package.rb
@@ -1,4 +1,11 @@
 class Package < ApplicationRecord
    belongs_to :receiver
    belongs_to :sender
+
+   validates :weight, presence: true
+   validates_numericality_of :weight, only_integer: true, greater_than: 0
+   validates :service_provider, presence: true
+   validates_inclusion_of :service_provider, in: ["UPS", "FedEx", "USPS"]
+   validates :service, presence: true
+
 end
--- app/models/sender.rb
+++ app/models/sender.rb
@@ -1,4 +1,25 @@
 class Sender < ApplicationRecord
    has_many :packages 
    has_many :receivers, through: :packages
+
+   validate :has_company_or_firstname_lastname
+   validates :street1, presence: true
+   validates :city, presence: true
+   validates :state, presence: true 
+   validates :zip, presence: true
+   validates_numericality_of :zip, only_integer: true, greater_than: 0, less_than: 99999
+
+   #Custom Validator
+   def has_company_or_firstname_lastname
+      has_name = !!self.firstname && !!self.lastname
+      has_company = !!self.company
+
+      result = has_name || has_company
+      if result != true
+         errors.add(:name_or_company_validation, "Must have Company or Firstname & Lastname")
+      end
+   end
+
+
+   
 end
