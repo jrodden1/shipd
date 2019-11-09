a05546d888c50a48cf6b5d7a20e35b0ba90a48da
Jeremiah Rodden <jrodden1@gmail.com>
2019-11-09 11:19:25 -0700

    Removed commented out code from App.js

--- client/src/App.js
+++ client/src/App.js
@@ -25,30 +25,3 @@ class App extends Component {
 }
 
 export default App;
-  
-  //   return (
-  //     <div className="App">
-  //       <header className="App-header">
-  //         <img src={shipdLogo} className="App-logo" alt="logo" />
-  //         <p>
-  //           <code>PUT CODE HERE!!</code>
-  //         </p>
-  //         <link
-  //           rel="stylesheet"
-  //           href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-  //           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-  //           crossOrigin="anonymous"
-  //         />
-  
-  //         <a
-  //           className="App-link"
-  //           href="https://reactjs.org"
-  //           target="_blank"
-  //           rel="noopener noreferrer"
-  //         >
-  //           Learn React
-  //         </a>
-  //       </header>
-  //     </div>
-  //   );
-  // }
