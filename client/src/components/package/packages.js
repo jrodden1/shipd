217481e028c96399b5bc7b320403f1c63b46fad9
Jeremiah Rodden <jrodden1@gmail.com>
2019-11-09 11:56:46 -0700

    Cleaning up code in Packages.js

--- client/src/components/package/packages.js
+++ client/src/components/package/packages.js
@@ -5,12 +5,9 @@ import { CardDeck } from 'react-bootstrap'
 
 const Packages = ({ packages, createPackage, deletePackage, history, setModalShow, modalShow }) => {
    const formatPackages = () => {
-      //REFACTOR ; Code clean up.  I can remove this console log and make this a one line arrow function depending upon formatting.
-      console.log("formatPackages", packages)
       return packages.map(pkg => <Package key={pkg.id} pkg={pkg} deletePackage={deletePackage} setModalShow={setModalShow} modalShow={modalShow} />)
    }
    
-   console.log("Packages comp packages prop", packages)
    return (
       <div className="text-center">
          <Link to="/packages/new" className="btn btn-secondary">Create New Package</Link><br /><br />
