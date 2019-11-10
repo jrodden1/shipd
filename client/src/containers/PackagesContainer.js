692f7c3afad071f564f7176bebf50405fabb2b6b
Jeremiah Rodden <jrodden1@gmail.com>
2019-11-09 22:33:11 -0700

    Wired up Loading component for PackagesContainer

--- client/src/containers/PackagesContainer.js
+++ client/src/containers/PackagesContainer.js
@@ -7,6 +7,7 @@ import PackageDetail from '../components/package/PackageDetail'
 import DeletePackageModal from '../components/package/DeletePackageModal'
 import PackageForm from '../components/package/PackageForm'
 import serviceProviders from '../helpers/serviceProviderHelpers'
+import Loading from '../helpers/Loading.js'
 
 class PackagesContainer extends Component {
    constructor(props) {
@@ -38,24 +39,31 @@ class PackagesContainer extends Component {
    }
 
    render() {
-      console.log("Packages Container Props", this.props)
+      const { loadingPackages, deletingPackage, creatingPackage } = this.props
       return (
-         <div>
+         <React.Fragment>
             <Switch>
                <Route exact path={this.props.match.url} render={() => {
-                  return (
-                     <React.Fragment>
-                        <br/>
-                        <Packages 
-                           packages={this.props.packages} 
-                           createPackage={this.props.createPackage}
-                           deleteModalShow={this.deleteModalShow}
-                           setModalShow={this.setModalShow}
-                           modalShow={this.state.modalShow}
-                           history={this.props.history}
-                        />
-                     </React.Fragment>
-                  )}}
+                  
+                  if (loadingPackages || deletingPackage || creatingPackage) {
+                     return (
+                        <Loading />
+                     )
+                  } else {
+                     return (
+                        <React.Fragment>
+                           <br/>
+                           <Packages 
+                              packages={this.props.packages} 
+                              createPackage={this.props.createPackage}
+                              deleteModalShow={this.deleteModalShow}
+                              setModalShow={this.setModalShow}
+                              modalShow={this.state.modalShow}
+                              history={this.props.history}
+                           />
+                        </React.Fragment>
+                     )}}
+                  }
                />
                <Route exact path={`${this.props.match.url}/new`} render={() => {
                   return (
@@ -67,30 +75,35 @@ class PackagesContainer extends Component {
                   )}} 
                />
                <Route path={`${this.props.match.url}/:packageId`} render={routerProps => {
-                  console.log("Route - by Id")
-                  //grab just the specific package and pass it down to the packages prop... I suppose this logic could be passed down to the Packages component.  REFACTOR
-                  const pkg = this.props.packages.filter(pack => pack.id === parseInt(routerProps.match.params.packageId))
-                  return (
-                     <React.Fragment>
-                        <br/>
-                        <Packages 
-                           {...routerProps} 
-                           createPackage={this.props.createPackage} 
-                           packages={pkg}
-                           deleteModalShow={this.deleteModalShow}
-                           setModalShow={this.setModalShow}
-                           modalshow={this.modalShow}
-                           history={this.props.history}
-                        />
-                        <div className="text-center">
-                           <Link
-                              to="/packages"
-                              className="btn btn-secondary"
-                           >Back to All Packages
-                           </Link>
-                        </div>
-                     </React.Fragment>
-                  )}}
+                  if (loadingPackages || deletingPackage || creatingPackage) {
+                     return (
+                        <Loading />
+                     )
+                  } else {
+                     //grab just the specific package and pass it down to the packages prop... I suppose this logic could be passed down to the Packages component.  REFACTOR
+                     const pkg = this.props.packages.filter(pack => pack.id === parseInt(routerProps.match.params.packageId))
+                     return (
+                        <React.Fragment>
+                           <br/>
+                           <Packages 
+                              {...routerProps} 
+                              createPackage={this.props.createPackage} 
+                              packages={pkg}
+                              deleteModalShow={this.deleteModalShow}
+                              setModalShow={this.setModalShow}
+                              modalshow={this.modalShow}
+                              history={this.props.history}
+                           />
+                           <div className="text-center">
+                              <Link
+                                 to="/packages"
+                                 className="btn btn-secondary"
+                              >Back to All Packages
+                              </Link>
+                           </div>
+                        </React.Fragment>
+                     )}}
+                  }
                />
             </Switch>
             <PackageDetail 
@@ -105,7 +118,7 @@ class PackagesContainer extends Component {
                deletePackage={this.props.deletePackage}
                history={this.props.history}
             />
-         </div>
+         </React.Fragment>
       )
    }
 }
@@ -113,7 +126,10 @@ class PackagesContainer extends Component {
 const mapStateToProps = store => {
    console.log("StoreState", store)
    return {
-      packages: store.packagesReducer.packages
+      packages: store.packagesReducer.packages,
+      loadingPackages: store.packagesReducer.loadingPackages,
+      creatingPackage: store.packagesReducer.creatingPackage,
+      deletingPackage: store.packagesReducer.deletingPackage
    }
 }
 
