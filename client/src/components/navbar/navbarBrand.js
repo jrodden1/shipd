a185c86d6c985a5b59442c06be7c762002bd272a
Jeremiah Rodden <jrodden1@gmail.com>
2019-11-09 23:41:58 -0700

    Added ability to click on Shipd logo and navigate to Home

--- client/src/components/navbar/navbarBrand.js
+++ client/src/components/navbar/navbarBrand.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import shipdLogo from '../../shipd.svg'
 import { Navbar } from 'react-bootstrap'
+import { NavLink } from 'react-router-dom'
 
 const navbarBrand = () => {
    return (
-      <Navbar.Brand href="#home">
-         <img
-         alt=""
-         src={shipdLogo}
-         width="45"
-         height="45"
-         className="d-inline-block align-center"
-         />
-         {`       Shipd`}
-      </Navbar.Brand>
+      <NavLink to="/">
+         <Navbar.Brand>
+            <img
+            alt=""
+            src={shipdLogo}
+            width="45"
+            height="45"
+            className="d-inline-block align-center"
+            />
+            {`       Shipd`}
+         </Navbar.Brand>
+      </NavLink>
    );
 }
 
